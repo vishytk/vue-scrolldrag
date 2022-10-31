088d8414a158a16ad509e02cd9b7e5ed5902aabf
vishytk <vishwanath.tk@gmail.com>
2022-10-31 16:21:32 +0530

    use cursor-move

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const doSomething = (e) => {
     <div v-dragscroll class="overflow-scroll w-[800px] h-[800px]">
       <img
         src="https://images.pexels.com/photos/13377187/pexels-photo-13377187.jpeg"
-        class="object-none object-cover w-full h-full"
+        class="object-none object-cover w-full h-full cursor-move"
       />
     </div>
   </div>
